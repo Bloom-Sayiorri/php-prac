d30aadcdc99845c727c75e2dc5a1c5f5bec8778d
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-02-02 14:55:00 +0300

    forms in php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <input type="checkbox" name="taco" value="Taco">Taco<br>
+        <input type="checkbox" name="hamburger" value="Hamburger">Hamburger<br>
+        <input type="checkbox" name="hotdog" value="Hotdog">Hotdog<br>
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
+
 <?php
-    echo("Hello World!");
+    // echo("Hello World!");
 ?>
