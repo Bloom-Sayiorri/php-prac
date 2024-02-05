828652635a9b42c4229bfeb1e46a3d0c6f307036
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-02-05 13:14:23 +0300

    form changes

--- index.php
+++ index.php
@@ -1,23 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="index.php" method="post">
-        <p>Select your favourite snacks;</p>
-        <input type="radio" name="food" value="Taco">Taco<br>
-        <input type="radio" name="food" value="Hamburger">Hamburger<br>
-        <input type="radio" name="food" value="Hotdog">Hotdog<br>
+        <p>Input your country to see your capital city:</p>
+        <input type="text" name="country" />
         <input type="submit" name="submit" value="submit">
     </form>
 </body>
+
 </html>
 
 <?php
-    if(isset($_POST["submit"])) {
-        echo "You tried to submit";
-    }
+$capitals = [
+    "USA" => "Washington D.C",
+    "Kenya" => "Nairobi",
+    "Japan" => "Tokyo",
+    "India" => "New Delhi"
+];
+
+$country = $capitals[$_POST["country"]];
+echo $country;
+// foreach($capitals as $key => $value){
+//     echo "Country: $key<br>,
+//      Capital: $value <br>";
+// }
 ?>
