040dc86ed5bdffa8d3d5571e6ec6e64b461c2ee8
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-02-05 15:31:13 +0300

    form changes

--- index.php
+++ index.php
@@ -1,34 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="index.php" method="post">
-        <p>Input your country to see your capital city:</p>
-        <input type="text" name="country" />
-        <input type="submit" name="submit" value="submit">
-    </form>
-</body>
-
+    <body>
+        <form action="index.php" method="post">
+            <p>Input your country to see your capital city:</p>
+            <input type="text" name="country" />
+            <input type="submit" name="submit" value="submit">
+        </form>
+    </body>
 </html>
 
 <?php
-$capitals = [
-    "USA" => "Washington D.C",
-    "Kenya" => "Nairobi",
-    "Japan" => "Tokyo",
-    "India" => "New Delhi"
-];
+    $capitals = [
+        "USA" => "Washington D.C",
+        "Kenya" => "Nairobi",
+        "Japan" => "Tokyo",
+        "India" => "New Delhi"
+    ];
 
-$country = $capitals[$_POST["country"]];
-echo $country;
-// foreach($capitals as $key => $value){
-//     echo "Country: $key<br>,
-//      Capital: $value <br>";
-// }
+    $country = $capitals[$_POST["country"]];
+    echo $country;
 ?>
